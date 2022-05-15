92edfd2b4847e45bc94aba414987adc30319a47c
KyeoRaeHan <ray56260201@gmail.com>
2022-05-15 19:19:47 +0900

    메인페이지 애니메이션css 추가

--- components/Home.js
+++ components/Home.js
@@ -1,3 +1,4 @@
+// import styles from "@/styles/Fadein.module.css";
 import styles from "@/styles/Home.module.css";
 
  
@@ -5,18 +6,37 @@ import styles from "@/styles/Home.module.css";
 
 export function Home() {
     return (
-        // <div className={styles.gallarylist}>       
-            // <div className={styles.user-wrap}>
-            //     <div className={styles.user-text}>Pet Care Service</div>
-                // <div className={styles.user-text}>By AI Studios</div>
-                <img
+        <>
+        <div className={styles.textOnImg}>
+            <div className={styles.backgroundWrap}>
+                <div className={styles.content}>
+                    <span>Pet Care System</span>
+                    <span>by AI Studios</span>
+                </div>
+            </div>
+        </div>
+
+        {/* <div> 
+        <div>123</div>
+              <img
                     // className={styles.user-image}   
                     src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbx5UIl%2FbtrBT5DYw1i%2FQ3jicEc9nSbFkK1tOlK7k0%2Fimg.png"
                     width="1700"
                     height="800"
-                />           
-            // </div>
-        
-        // </div>
+                />      
+        </div>  */}
+            
+           
+            {/* <div className={styles.user-wrap}>
+                <div className={styles.user-text}>Pet Care Service</div>
+                // <div className={styles.user-text}>By AI Studios</div> */}
+                 {/* <img
+                    // className={styles.user-image}   
+                    src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbx5UIl%2FbtrBT5DYw1i%2FQ3jicEc9nSbFkK1tOlK7k0%2Fimg.png"
+                    width="1700"
+                    height="800"
+                />            
+         </div> */}
+        </>
     );
 }
